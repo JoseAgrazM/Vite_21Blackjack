d2e4caea49fd10d26544634729bb4accbbd25780
Jose Agraz <joseagrazm@gmail.com>
2024-01-13 01:37:37 +0100

    Delete folder

--- src/blackjack/usercases/acumular-puntos.js
+++
@@ -1,7 +0,0 @@
-import { valorCarta } from "./valor-carta";
-
-export const acumularPuntos = (carta, turno) => {
-	puntosJugadores[turno] = puntosJugadores[turno] + valorCarta(carta);
-	puntosHTML[turno].innerText = puntosJugadores[turno];
-	return puntosJugadores[turno];
-};
